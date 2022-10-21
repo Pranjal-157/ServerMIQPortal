3648331e0f23f88878059ada98c58b938d080682
Richard2415 <richardselvaraj07@gmail.com>
2022-10-21 13:47:02 +0530

    db changed

--- db.js
+++ db.js
@@ -4,9 +4,12 @@ dotenv.config();
 
 
 module.exports = () => {
- 
+  const connectParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
   try{
-    mongoose.connect(process.env.DB)
+    mongoose.connect(process.env.DB, connectParams)
     console.log('Connected to database successfully')
   } catch(error){
     console.log(error);

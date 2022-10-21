276337e6562850a8501dfea4add7baa5400d0394
Richard2415 <richardselvaraj07@gmail.com>
2022-10-21 13:51:57 +0530

    config changed

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { User } = require("../models/user");
 const Joi = require("joi");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({ path: '/custom/path/to/.env' });
 
 router.post("/", async (req, res) => {
   try {
